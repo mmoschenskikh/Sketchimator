a5ba525ed6cd552407923b532523ba6de2545299
Maxim Moschenskikh <moschenskikh@yandex-team.ru>
2024-10-30 16:31:03 +0300

    Dimension tokens added

---
+++ app/src/main/java/ru/maxultra/sketchimator/ui/theme/tokens/DimenTokens.kt
@@ -0,0 +1,18 @@
+package ru.maxultra.sketchimator.ui.theme.tokens;
+
+import androidx.compose.ui.unit.dp
+
+object DimenTokens {
+
+    private val base = 4.dp
+
+    val Zero = 0.dp
+    val x1 = base // 4
+    val x2 = base * 2 // 8
+    val x3 = base * 3 // 12
+    val x4 = base * 4 // 16
+    val x5 = base * 5 // 20
+    val x6 = base * 6 // 24
+    val x7 = base * 7 // 28
+    val x8 = base * 8 // 32
+}
