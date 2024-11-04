7423f2f73a979e8712db5ffa64289c1e90f2b909
Maxim Moschenskikh <moschenskikh@yandex-team.ru>
2024-11-04 19:58:43 +0300

    Generation of rotating triangles

--- app/src/main/java/ru/maxultra/sketchimator/feature_frame_generation/impl/MovingTriangleGenerator.kt
+++ app/src/main/java/ru/maxultra/sketchimator/feature_frame_generation/impl/MovingTriangleGenerator.kt
@@ -1,12 +1,13 @@
 package ru.maxultra.sketchimator.feature_frame_generation.impl
 
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Matrix
 import androidx.compose.ui.graphics.copy
+import androidx.compose.ui.graphics.setFrom
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.toOffset
+import androidx.core.graphics.rotationMatrix
 import ru.maxultra.sketchimator.DrawParameters
 import ru.maxultra.sketchimator.Frame
 import ru.maxultra.sketchimator.PathWithParameters
@@ -17,6 +18,7 @@ import ru.maxultra.sketchimator.util.PathUtils.isOutOfBoundsByX
 import ru.maxultra.sketchimator.util.PathUtils.isOutOfBoundsByY
 import kotlin.math.sign
 import kotlin.random.Random
+
 
 class MovingTriangleGenerator(
     private val frameSize: IntSize,
@@ -41,21 +43,37 @@ class MovingTriangleGenerator(
 
         var offsetX = getRandomOffset()
         var offsetY = getRandomOffset()
+        val rotationMatrix = Matrix()
 
+        var rotation = getRandomRotation(Random.nextBoolean())
         val frames = Array(frameCount) {
+            triangle = triangle.copy()
+            val bounds = triangle.getBounds()
+            rotationMatrix.setFrom(
+                rotationMatrix(
+                    degrees = rotation,
+                    px = bounds.center.x,
+                    py = bounds.center.y,
+                )
+            )
+
+            triangle.transform(rotationMatrix)
+
             offsetX = if (triangle.isOutOfBoundsByX(frameSize, offsetX)) {
+                rotation = getRandomRotation(offsetY < 0)
                 getRandomOffset() * -sign(offsetX)
             } else {
                 offsetX
             }
 
             offsetY = if (triangle.isOutOfBoundsByY(frameSize, offsetY)) {
+                rotation = getRandomRotation(offsetX < 0)
                 getRandomOffset() * -sign(offsetY)
             } else {
                 offsetY
             }
 
-            triangle = triangle.copy().apply { translate(Offset(offsetX, offsetY)) }
+            triangle.translate(Offset(offsetX, offsetY))
             Frame(
                 drawnPaths = listOf(PathWithParameters(triangle, parameters)),
                 undonePaths = emptyList(),
@@ -66,10 +84,21 @@ class MovingTriangleGenerator(
     }
 
     private fun getRandomOffset(): Float = Random.nextInt(MIN_OFFSET_PER_FRAME, MAX_OFFSET_PER_FRAME).toFloat()
+
+    private fun getRandomRotation(
+        clockwise: Boolean,
+    ): Float {
+        val lowerBound = if (clockwise) MIN_ROTATION_PER_FRAME else -MAX_ROTATION_PER_FRAME
+        val upperBound = if (clockwise) MAX_ROTATION_PER_FRAME else MIN_ROTATION_PER_FRAME
+        return Random.nextInt(lowerBound, upperBound).toFloat()
+    }
 }
 
 private const val MIN_OFFSET_PER_FRAME = 10
 private const val MAX_OFFSET_PER_FRAME = 50
+
+private const val MIN_ROTATION_PER_FRAME = 0
+private const val MAX_ROTATION_PER_FRAME = 30
 
 private const val POINT_OFFSET_LOWER_BOUND = 100
 private const val POINT_OFFSET_UPPER_BOUND = 200
